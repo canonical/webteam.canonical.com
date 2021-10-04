27a31e94b5b003cc692aad3f9926f4dbb6241ecc
ClementChaumel <clement.chaumel@gmail.com>
2021-10-04 16:12:09 +0200

    Create basic ball

--- eight-ball/src/App.svelte
+++ eight-ball/src/App.svelte
@@ -1,30 +1,64 @@
 <script>
-	export let name;
+  import Ball from "./Ball.svelte";
+  let answer = "No";
+
+  var words = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Do not count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+  ];
+
+  export function askQuestion() {
+    answer = words[Math.floor(Math.random() * words.length)];
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>APP NAME GOES HERE</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+  <input type="text" placeholder="Ask a question to the magic 8 ball" />
+  <button on:click={askQuestion}>Ask</button>
+  <Ball {answer} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
---
+++ eight-ball/src/Ball.svelte
@@ -0,0 +1,47 @@
+<script>
+  export let answer = "Yes";
+</script>
+
+<div class="ball">
+  <div class="hole">
+    <div class="triangle">
+      <p class="text">
+        {answer}
+      </p>
+    </div>
+  </div>
+</div>
+
+<style>
+  .ball {
+    width: 500px;
+    height: 500px;
+    border-radius: 50%;
+    background-color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .hole {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    background-color: white;
+  }
+
+  .triangle {
+    width: 0;
+    height: 0;
+    border-left: 100px solid transparent;
+    border-right: 100px solid transparent;
+    border-bottom: 150px solid blue;
+    display: flex;
+    justify-content: center;
+  }
+  .text {
+    color: white;
+    font-size: 50px;
+    font-weight: bold;
+    text-align: center;
+  }
+</style>
