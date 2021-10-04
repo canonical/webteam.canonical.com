dccd50f6a5ad468f03a253f7044f67a96157d1df
Thomas Bille <toto@canonical.com>
2021-10-04 12:56:17 +0200

    Add canonicool blueprint

--- webapp/app.py
+++ webapp/app.py
@@ -4,6 +4,7 @@ from canonicalwebteam.flask_base.app import FlaskBase
 from webapp.guides import discourse_docs
 from webapp.team import webteam
 from webapp.releases import releases
+from webapp.canonicool import canonicool
 
 app = FlaskBase(
     __name__,
@@ -20,4 +21,5 @@ def index():
 
 app.register_blueprint(webteam, url_prefix="/team")
 app.register_blueprint(releases, url_prefix="/releases")
+app.register_blueprint(canonicool, url_prefix="/canonicool")
 discourse_docs.init_app(app)
