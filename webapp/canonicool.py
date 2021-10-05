94a199b85e91447a99c0efa795f5169a0f3e7762
Beth Collins <bethcollinsliv92@gmail.com>
2021-10-05 14:52:40 +0100

    Humanize date and add pagination

--- webapp/canonicool.py
+++ webapp/canonicool.py
@@ -1,13 +1,14 @@
 import requests
 import os
+import humanize
 
 from datetime import datetime
 from dateutil import parser
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 DEPLOYMENT_ID = os.getenv(
     "DEPLOYMENT_ID",
-    "AKfycbwahRHxgFZQSDCYn-jiSKf8ihsX1yBpdLtFOjTvADMp3fRpJ4dCuVU1OxX_rn4793ZY",
+    "AKfycbwM5X59etlXHY8zfl-TnHIHF9B6Wi_-w0dq4Q1OhirunKpitzhAAezKtJAGeUYsHaXz",
 )
 CANONICOOL_SHEET_URL = (
     f"https://script.google.com/macros/s/{DEPLOYMENT_ID}/exec"
@@ -23,6 +24,9 @@ canonicool = Blueprint(
 
 @canonicool.route("/")
 def index():
+    limit = request.args.get("limit", default=6, type=int)
+    offset = request.args.get("offset", default=0, type=int)
+    page = request.args.get('page', default=1, type=int)
     response = requests.get(CANONICOOL_SHEET_URL)
 
     future_events = []
@@ -32,11 +36,12 @@ def index():
 
     for canonicool_session in response.json():
         session_date = parser.parse(canonicool_session["date"])
+        canonicool_session["human_date"] = humanize.naturalday(session_date)
         if today > session_date.replace(tzinfo=None):
-            past_events.append(canonicool_session)
+            past_events.insert(0, canonicool_session)
         elif today <= session_date.replace(tzinfo=None):
             future_events.append(canonicool_session)
 
     return render_template(
-        "canonicool.html", past_events=past_events, future_events=future_events
+        "canonicool.html", past_events=past_events, future_events=future_events, page=page, limit=limit, offset=offset
     )
