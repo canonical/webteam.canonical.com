bb1cf698ab237589b34a5b14e370ddd735dbfd1b
andesol <anddessol@gmail.com>
2021-10-05 18:53:47 +0200

    styles create page

--- eight-ball/src/Create.svelte
+++ eight-ball/src/Create.svelte
@@ -19,9 +19,11 @@
   };
 </script>
 
-<main>
-  <h1>CREATE YOUR OWN MAGIC 8 BALL!!!!!!!!!!</h1>
-  <h3>Add you own answers</h3>
+<main class="container">
+  <header>
+    <h1>CREATE YOUR OWN MAGIC 8 BALL!!!!!!!!!!</h1>
+    <p>Add your own answers</p>
+  </header>
   <input
     type="text"
     id="answer"
@@ -30,16 +32,73 @@
     bind:this={answerInput}
     on:keypress={onKeyPress}
   />
-  <button on:click={addAnswer} disabled={!answer} type="submit">Add</button>
-  {#each answerList as answer, i}
-    <li>
-      {answer}
-      <button
-        on:click={() => {
-          removeItem(i);
-        }}>X</button
-      >
-    </li>
-  {/each}
-  <Link to="/?answers={answerList.toString()}"><button>GENERATE</button></Link>
+  <button
+    on:click={addAnswer}
+    disabled={!answer}
+    type="submit"
+    class="btn btn--success">Add</button
+  >
+  <Link to="/?answers={answerList.toString()}"
+    ><button class="btn">Generate</button></Link
+  >
+  <ul class="list">
+    {#each answerList as answer, i}
+      <li class="list__item">
+        {answer}
+        <button
+          on:click={() => {
+            removeItem(i);
+          }}>X</button
+        >
+      </li>
+    {/each}
+  </ul>
 </main>
+
+<style>
+  header {
+    padding: 2rem 0rem;
+  }
+  .container {
+    width: 90%;
+    max-width: 900px;
+    margin: 0 auto;
+  }
+
+  .btn {
+    padding: 0.25em 0.75em;
+    min-width: 10ch;
+    min-height: 44px;
+    border-radius: 0.5rem;
+    font-size: 1.2rem;
+  }
+
+  .btn--success {
+    background-color: green;
+    color: #fff;
+  }
+
+  input {
+    font-size: 16px;
+    font-family: inherit;
+    padding: 0.25em 0.5em;
+    border-radius: 0.5rem;
+    transition: 180ms box-shadow ease-in-out;
+    min-height: 44px;
+    width: 50%;
+  }
+
+  input:focus {
+    outline: 3px solid transparent;
+    box-shadow: 0 0 3px orange;
+  }
+
+  .list__item {
+    display: flex;
+    justify-content: space-between;
+    background: white;
+    color: black;
+    font-size: 2rem;
+    padding: 0.5rem 1.2rem;
+  }
+</style>
