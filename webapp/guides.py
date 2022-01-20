a0842c037a8be61e20944808123f7ca5116edf0b
Thomas Bille <toto@canonical.com>
2022-01-20 12:55:20 +0100

    Update docs & add login

--- webapp/guides.py
+++ webapp/guides.py
@@ -1,18 +1,34 @@
+import os
+from canonicalwebteam.discourse import DiscourseAPI, DocParser, Docs
+import flask
+
 import talisker.requests
 
-from canonicalwebteam.discourse import DiscourseAPI, DocParser, Docs
+DISCOURSE_INDEX_TOPIC = 157
+DISCOURSE_API_KEY = os.getenv("DISCOURSE_API_KEY")
+DISCOURSE_API_USERNAME = os.getenv("DISCOURSE_API_USERNAME")
 
-discourse_index_id = 14129
 
-session = talisker.requests.get_session()
-discourse_docs = Docs(
-    parser=DocParser(
-        api=DiscourseAPI(
-            base_url="https://discourse.ubuntu.com/", session=session
+def init_docs(app):
+    session = talisker.requests.get_session()
+    discourse_docs = Docs(
+        parser=DocParser(
+            api=DiscourseAPI(
+                base_url="https://discourse.canonical.com/",
+                session=session,
+                api_key=DISCOURSE_API_KEY,
+                api_username=DISCOURSE_API_USERNAME,
+            ),
+            index_topic_id=DISCOURSE_INDEX_TOPIC,
+            url_prefix="/guides",
         ),
-        index_topic_id=discourse_index_id,
+        document_template="guides.html",
         url_prefix="/guides",
-    ),
-    document_template="guides.html",
-    url_prefix="/guides",
-)
+    )
+
+    @discourse_docs.blueprint.before_request
+    def before_request():
+        if "openid" not in flask.session:
+            return flask.redirect("/login?next=" + flask.request.path)
+
+    discourse_docs.init_app(app)
