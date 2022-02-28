32b64f5e2614ea399635261d057865b9e7d76c79
Scott Mason Nash <sowasred2012@gmail.com>
2022-02-28 17:39:54 +0000

    add design assembly view

--- webapp/app.py
+++ webapp/app.py
@@ -6,6 +6,7 @@ from webapp.sso import init_sso
 from webapp.team import webteam
 from webapp.releases import releases
 from webapp.canonicool import canonicool
+from webapp.design_assembly import design_assembly
 
 app = FlaskBase(
     __name__,
@@ -23,5 +24,6 @@ def index():
 app.register_blueprint(webteam, url_prefix="/team")
 app.register_blueprint(releases, url_prefix="/releases")
 app.register_blueprint(canonicool, url_prefix="/canonicool")
+app.register_blueprint(design_assembly, url_prefix="/design-assembly")
 init_docs(app)
 init_sso(app)
