d36b100318aa52e727c245dd8ffa6357cc8bbd05
Scott Mason Nash <sowasred2012@gmail.com>
2022-03-01 12:02:59 +0000

    add sso to design-assembly

--- webapp/sso.py
+++ webapp/sso.py
@@ -71,3 +71,19 @@ def init_sso(app):
                 response.headers["Cache-Control"] = "private"
 
         return response
+
+
+def login_required(func):
+    """
+    Decorator that checks if a user is logged in, and redirects
+    to login page if not.
+    """
+
+    @functools.wraps(func)
+    def is_user_logged_in(*args, **kwargs):
+        if "openid" not in flask.session:
+            return flask.redirect("/login?next=" + flask.request.path)
+
+        return func(*args, **kwargs)
+
+    return is_user_logged_in
