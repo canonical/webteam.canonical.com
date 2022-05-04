c4fd614f4b1a1be9d6633535ddd2a8fc6d954cdc
Anthony Dillon <me@anthonydillon.com>
2022-05-04 14:32:08 +0200

    Add basic Masterclasses view to the site

---
+++ webapp/masterclasses.py
@@ -0,0 +1,91 @@
+from datetime import datetime
+from flask import Blueprint, render_template
+from webapp.spreadsheet import get_sheet
+
+
+SPREADSHEET_ID = "1fFumFWIM3oHwLr9pcBlaANcadAU0bNJxbkfGKwC_1pg"
+sheet = get_sheet()
+
+masterclasses = Blueprint(
+    "masterclasses",
+    __name__,
+    template_folder="/templates",
+    static_folder="/static",
+)
+
+
+def get_value_row(row, type):
+    if row:
+        if type == datetime:
+            if "formattedValue" in row:
+                return datetime.strptime(
+                    row["formattedValue"], "%d %B %Y"
+                ).strftime("%d %b %Y")
+        elif "userEnteredValue" in row:
+            if "stringValue" in row["userEnteredValue"]:
+                if "link" in row["userEnteredFormat"]["textFormat"]:
+                    return row["userEnteredFormat"]["textFormat"]["link"][
+                        "uri"
+                    ]
+                return type(row["userEnteredValue"]["stringValue"])
+            if "numberValue" in row["userEnteredValue"]:
+                return type(row["userEnteredValue"]["numberValue"])
+
+    return ""
+
+
+def index_in_list(a_list, index):
+    return index < len(a_list)
+
+
+# Parse the id from a google url
+# https://drive.google.com/open?id=15cy6HFRkzgidDQ1Ff1wBDP-TVihfg_15
+# https://drive.google.com/file/d/1YkOAdSwmAHmddwhQ2l0sbEQyflbkhQ5L/view
+# https://drive.google.com/file/d/1nCbTZtX380e6lD1kkQ6BjTA0aAknZJGY/view?usp=drivesdk
+# https://drive.google.com/file/d/1TQLA53RPhf20Q9BeqMN6e1DFb_kPsF91/view?usp=drive_web
+def get_id(video_link):
+    if video_link:
+        if "id=" in video_link:
+            return video_link.split("id=")[1]
+        elif "file/d/" in video_link:
+            return video_link.split("file/d/")[1].split("/view")[0]
+    return ""
+
+
+@masterclasses.route("/")
+def index():
+    SHEET = "Completed"
+    RANGE = "A2:G1000"
+    COLUMNS = [
+        ("Topic", str),
+        ("Owner", str),
+        ("Duration", str),
+        ("Date", datetime),
+        ("Slides", str),
+        ("Recording", str),
+        ("Description", str),
+    ]
+    res = sheet.get(
+        spreadsheetId=SPREADSHEET_ID,
+        ranges=[f"{SHEET}!{RANGE}"],
+        includeGridData=True,
+    ).execute()
+
+    sessions = []
+    for row in res["sheets"][0]["data"][0]["rowData"]:
+        if "values" in row and row["values"][0]:
+            session = {}
+            for column_index in range(len(COLUMNS)):
+                (column, type) = COLUMNS[column_index]
+                session[column] = get_value_row(
+                    row["values"][column_index]
+                    if index_in_list(row["values"], column_index)
+                    else None,
+                    type,
+                )
+                if COLUMNS[column_index][0] == "Recording":
+                    session["Link"] = get_id(session[column])
+
+            sessions.append(session)
+
+    return render_template("masterclasses.html", sessions=sessions)
---
+++ webapp/spreadsheet.py
@@ -0,0 +1,66 @@
+import os
+import hashlib
+import tempfile
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+SERVICE_ACCOUNT_FILE = "client.json"
+PRIVATE_KEY_ID = os.getenv("PRIVATE_KEY_ID")
+PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+
+
+class DiscoveryCache:
+    """
+    Unix file-based cache for use with the API Discovery service
+    See https://github.com/googleapis/google-api-python-client/issues/325#issuecomment-419387788
+    """  # noqa
+
+    def filename(self, url):
+        return os.path.join(
+            tempfile.gettempdir(),
+            "google_api_discovery_" + hashlib.md5(url.encode()).hexdigest(),
+        )
+
+    def get(self, url):
+        try:
+            with open(self.filename(url), "rb") as f:
+                return f.read().decode()
+        except FileNotFoundError:
+            return None
+
+    def set(self, url, content):
+        with tempfile.NamedTemporaryFile(delete=False) as f:
+            f.write(content.encode())
+            f.flush()
+            os.fsync(f)
+        os.rename(f.name, self.filename(url))
+
+
+def get_sheet():
+    service_account_info = {
+        "type": "service_account",
+        "project_id": "roadmap-270011",
+        "private_key_id": PRIVATE_KEY_ID,
+        "private_key": PRIVATE_KEY,
+        "client_email": "specs-reader@roadmap-270011.iam.gserviceaccount.com",
+        "client_id": "112404606310881291739",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": (
+            "https://www.googleapis.com/oauth2/v1/certs"
+        ),
+        "client_x509_cert_url": (
+            "https://www.googleapis.com/robot/v1/metadata"
+            "/x509/specs-reader%40roadmap-270011.iam.gserviceaccount.com"
+        ),
+    }
+
+    creds = service_account.Credentials.from_service_account_info(
+        service_account_info, scopes=SCOPES
+    )
+    service = build("sheets", "v4", credentials=creds, cache=DiscoveryCache())
+    sheet = service.spreadsheets()
+
+    return sheet
