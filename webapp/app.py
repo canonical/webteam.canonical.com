e6b25a4847b921bdcf7cb326e3137682c5056618
Aaryan Porwal <aaryan.porwal@canonical.com>
2024-08-28 15:34:16 +0530

    fix(links) Fix masterclasses and guides link

--- webapp/app.py
+++ webapp/app.py
@@ -3,7 +3,6 @@ from canonicalwebteam.flask_base.app import FlaskBase
 
 from webapp.blog_guild import blog_guild
 from webapp.canonicool import canonicool
-from webapp.masterclasses import masterclasses
 from webapp.design_assembly import design_assembly
 from webapp.practices import bootstrap_practices
 from webapp.releases import releases
@@ -29,7 +28,6 @@ app.register_blueprint(webteam, url_prefix="/team")
 app.register_blueprint(releases, url_prefix="/releases")
 app.register_blueprint(blog_guild, url_prefix="/blog-guild")
 app.register_blueprint(canonicool, url_prefix="/canonicool")
-app.register_blueprint(masterclasses, url_prefix="/masterclasses")
 app.register_blueprint(design_assembly, url_prefix="/design-assembly")
 bootstrap_practices(app)
 init_sso(app)
